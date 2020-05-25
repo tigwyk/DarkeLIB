bdb74d99aee950a38be976f4c7c9a0d7912f55bb
Lee Ingram <lee@soylentcola.com>
2020-05-24 20:14:49 -0700

    Adding lightsaber skills

---
+++ mudlib/std/skills/lightsaber.c
@@ -0,0 +1,14 @@
+inherit "/std/skills/skill.c";
+
+void create() {
+    ::create();
+    set_stat("force");
+    set_dev_cost(6);
+    set_fast_dev_cost(18);
+}
+
+void info() {
+message("help","This is skill with one and two-handed single-blade lightsaber weapons."
+	, this_player());
+}
+
