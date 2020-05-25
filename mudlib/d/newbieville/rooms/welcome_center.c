2fd9cb82c4d67063366da44aa2ac87a98e4f29cc
Lee Ingram <lee@soylentcola.com>
2020-05-24 18:25:21 -0700

    Fixing references

--- mudlib/d/newbieville/rooms/welcome_center.c
+++ mudlib/d/newbieville/rooms/welcome_center.c
@@ -9,11 +9,11 @@ void create() {
 
   ob = new("std/bboard");
   ob->set_name("board");
-  ob->set_id( ({"board", "castle board", "bets"}) );
-  ob->set_board_id("newbieville_castle_board");
+  ob->set_id( ({"board", "welcome center board", "bets"}) );
+  ob->set_board_id("newbieville_welcome_center_board");
   ob->set_max_posts(50);
-  ob->set_location(ROOMS+"castle");
-  ob->set("short","Newbieville Bulletin Board");
+  ob->set_location(ROOMS+"welcome_center");
+  ob->set("short","Newbieville Welcome Center Bulletin Board");
   ob->set("long",@TEXT
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 This board is meant to help newbies out as they first begin their journeys
@@ -31,8 +31,8 @@ TEXT
 
     set_property("light", 4);
     set_property("indoors", 1);
-    set("short", "The Castle of Tailwind");
-    set("long", "This is the Castle of Lord Tailwind. You see a bulletin board in the center of the room, a staircase leading upwards, and nothing else.\n%^B_RED%^If you are a new player you should type%^YELLOW%^up or 'u' for short%^RESET%^%^RESET%^\n\n you can also%^YELLOW%^'look sign'%^RESET%^for further directions");
+    set("short", "The Welcome Center");
+    set("long", "This is the Welcome Center set up by the local planetary council. You see a bulletin board in the center of the room, a staircase leading upwards, and nothing else.\n%^B_RED%^If you are a new player you should type%^YELLOW%^up or 'u' for short%^RESET%^%^RESET%^\n\n you can also%^YELLOW%^'look sign'%^RESET%^for further directions");
 
     add_exit(ROOMS+"upperfloor", "up");
     add_exit(ROOMS+"townsquare", "out");
